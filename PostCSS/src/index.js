a9ebbdb57db40b5dc07f69a4a8a2f7ef956c219e
longto <long.to@niteco.se>
2018-05-09 16:34:00 +0700

    postCSS

---
+++ PostCSS/src/index.js
@@ -0,0 +1,14 @@
+import './styles/fonts/index.css';
+import './styles/html.css';
+
+import './styles/index.css';
+import './styles/header.css';
+import './styles/navbar.css';
+import './styles/carousel.css';
+import './styles/container.css';
+import './styles/teaser.css';
+import './styles/footer.css';
+
+import { logSquare } from './scripts/logs';
+
+logSquare(3);
